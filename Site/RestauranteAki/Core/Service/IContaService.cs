c141ebcffde7d9721ee364251ad371c331694c86
Pedro Dantas <pedrood1232@gmail.com>
2025-08-16 21:33:16 -0300

    Add arquivo renomeado que não sido trackeado. #152

---
+++ Site/RestauranteAki/Core/Service/IContaService.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Service
+{
+    public interface IContaService
+    {
+        int Create(Contum conta);
+        void Edit(Contum conta);
+        void Delete(int id);
+        Contum? Get(int id);
+        IEnumerable<Contum> GetAll();
+    }
+}
