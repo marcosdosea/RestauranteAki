d7d5cf4df6a7a63cc4324d30809d8840feb6ac30
Pedro Dantas <pedrood1232@gmail.com>
2025-08-19 09:46:28 -0300

    Implementado service do pedido. Close #173

--- Site/RestauranteAki/Service/PedidoService.cs
+++ Site/RestauranteAki/Service/PedidoService.cs
@@ -1,33 +1,75 @@
 ﻿using Core;
 using Core.Service;
+using Microsoft.EntityFrameworkCore;
 
 namespace Service
 {
+    /// <summary>
+    /// Implementa serviços para manter dados do pedido
+    /// </summary>
     public class PedidoService : IPedidoService
     {
+        
+        private readonly RestauranteAkiContext context;
+
+        public PedidoService(RestauranteAkiContext context)
+        {
+            this.context = context;
+        }
+
+        /// <summary>
+        /// Criar um novo pedido na base de dados
+        /// </summary>
+        /// <param name="pedido">dados do pedido</param>
+        /// <returns>id do pedido</returns>
         public int Create(Pedido pedido)
         {
-            throw new NotImplementedException();
+            context.Add(pedido);
+            context.SaveChanges();
+            return pedido.Id;
         }
 
+        /// <summary>
+        /// Remover o pedido da base de dados
+        /// </summary>
+        /// <param name="id">id do pedido</param>
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            var pedido = context.Pedidos.Find(id);
+            if (pedido != null)
+            {
+                context.Remove(pedido);
+                context.SaveChanges();
+            }
         }
 
+        /// <summary>
+        /// Editar dados do pedido na base de dados
+        /// </summary>
+        /// <param name="pedido"></param>
         public void Edit(Pedido pedido)
         {
-            throw new NotImplementedException();
+            context.Update(pedido);
+            context.SaveChanges();
         }
 
+        /// <summary>
+        /// Buscar um pedido na base de dados
+        /// </summary>
+        /// <param name="id">id do pedido</param>
+        /// <returns>dados do pedido</returns>
         public Pedido? Get(int id)
         {
-            throw new NotImplementedException();
+            return context.Pedidos.Find(id);
         }
 
+        /// <summary>
+        /// Buscar todos os pedidos cadastrados
+        /// </summary>
+        /// <returns>lista de pedidos</returns>
         public IEnumerable<Pedido> GetAll()
         {
-            throw new NotImplementedException();
+            return context.Pedidos.AsNoTracking();
         }
     }
 }
