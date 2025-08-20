a9893af40ab5b517560a7e631392a33850d193b3
renneb777 <rennerox46@gmail.com>
2025-08-20 19:38:48 -0300

    Foi realizada a correção e a remoção dos atributos excedentes que não eram necessários. #181 Resolve

--- Site/RestauranteAki/RestauranteAkiWeb/Controllers/PedidoItemcardapioController.cs
+++ Site/RestauranteAki/RestauranteAkiWeb/Controllers/PedidoItemcardapioController.cs
@@ -9,27 +9,21 @@ namespace RestauranteWeb.Controllers
 {
     public class PedidoitemcardapioController : Controller
     {
-        private readonly IPedidoItemcardapioService pedidoitemcardapio;
-        private readonly IItemcardapioService itemCardapioService;
-        private readonly IPedidoService pedidoService;
+        private readonly IPedidoItemcardapioService pedidoitemcardapioService;
         private readonly IMapper mapper;
 
         public PedidoitemcardapioController(
-            IPedidoItemcardapioService pedidoitemcardapio,
-            IItemcardapioService itemCardapioService,
-            IPedidoService pedidoService,
+            IPedidoItemcardapioService pedidoitemcardapioService,
             IMapper mapper)
         {
-            this.pedidoitemcardapio = pedidoitemcardapio;
-            this.itemCardapioService = itemCardapioService;
-            this.pedidoService = pedidoService; 
+            this.pedidoitemcardapioService = pedidoitemcardapioService;
             this.mapper = mapper;
         }
 
         // GET: Pedidoitemcardapio
         public ActionResult Index()
         {
-            var listaPedidoitemcardapio = pedidoitemcardapio.GetAll();
+            var listaPedidoitemcardapio = pedidoitemcardapioService.GetAll();
             var PedidoitemcardapioViewModel = mapper.Map<List<PedidoItemcardapioViewModel>>(listaPedidoitemcardapio);
             return View(PedidoitemcardapioViewModel);
         }
@@ -48,7 +42,7 @@ namespace RestauranteWeb.Controllers
             if (ModelState.IsValid)
             {
                 var Pedidoitemcardapio = mapper.Map<PedidoItemcardapio>(pedidoitemcardapioViewModel);
-                pedidoitemcardapio.Create(Pedidoitemcardapio);
+                pedidoitemcardapioService.Create(Pedidoitemcardapio);
             }
             return RedirectToAction(nameof(Index));
         }
@@ -58,47 +52,47 @@ namespace RestauranteWeb.Controllers
         {
             if (ModelState.IsValid)
             {
-                var pedidoitemcardapio1 = mapper.Map<PedidoItemcardapio>(IdPedido);
-                pedidoitemcardapio.Edit(pedidoitemcardapio1);
+                var pedidoitemCardapio = mapper.Map<PedidoItemcardapio>(IdPedido);
+                pedidoitemcardapioService.Edit(pedidoitemCardapio);
             }
             return RedirectToAction(nameof(Index));
         }
 
-        // POST: Pedidoitemcardapio/Edit/5/10
+        // POST: Pedidoitemcardapio/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(PedidoItemcardapioViewModel pedidoitemcardapioViewModel)
         {
             if (ModelState.IsValid)
             {
-                var pedidoitemcardapio1 = mapper.Map<PedidoItemcardapio>(pedidoitemcardapioViewModel);
-                pedidoitemcardapio.Edit(pedidoitemcardapio1);
+                var pedidoitemCardapio = mapper.Map<PedidoItemcardapio>(pedidoitemcardapioViewModel);
+                pedidoitemcardapioService.Edit(pedidoitemCardapio);
             }
             return RedirectToAction(nameof(Index));
         }
 
-        // GET: Pedidoitemcardapio/Details/5/10
+        // GET: Pedidoitemcardapio/Details/5
         public ActionResult Details(int IdPedido)
         {
-            var pedidoItemCardapio = pedidoitemcardapio.Get(IdPedido);
-            var pedidoItemCardapioViewModel = mapper.Map<List<PedidoItemcardapioViewModel>>(pedidoItemCardapio);
+            var pedidoItemCardapio = pedidoitemcardapioService.Get(IdPedido);
+            var pedidoItemCardapioViewModel = mapper.Map<PedidoItemcardapioViewModel>(pedidoItemCardapio);
             return View(pedidoItemCardapioViewModel);
         }
 
-        // GET: Pedidoitemcardapio/Delete/5/10
+        // GET: Pedidoitemcardapio/Delete/5
         public ActionResult Delete(int IdPedido)
         {
-            var Pedidoitemcardapio = pedidoitemcardapio.Get(IdPedido);
+            var Pedidoitemcardapio = pedidoitemcardapioService.Get(IdPedido);
             var pedidoitemcardapioViewModel = mapper.Map<PedidoItemcardapioViewModel>(Pedidoitemcardapio);
             return View(pedidoitemcardapioViewModel);
         }
 
-        // POST: Pedidoitemcardapio/Delete/5/10
+        // POST: Pedidoitemcardapio/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(PedidoItemcardapioViewModel pedidoitemcardapio2)
+        public ActionResult Delete(int IdPedido, PedidoItemcardapioViewModel pedidoitemCardapio)
         {
-            pedidoitemcardapio.Delete(pedidoitemcardapio2.IdPedido);
+            pedidoitemcardapioService.Delete(IdPedido);
             return RedirectToAction(nameof(Index));
         }
     }
