f6df3e13b3fa25dd5c2a7a63a60bc071be5d59ac
TalyssonDEV <talyssonpestinha.TP@gmail.com>
2025-08-20 22:50:54 -0300

    Implementado Teste para Service e Controller da entidade itemcardapio. closes #209

---
+++ Site/RestauranteAki/RestauranteAkiWebTests/Controllers/ItemcardapioControllerTests.cs
@@ -0,0 +1,179 @@
+﻿using AutoMapper;
+using Core;
+using Core.Service;
+using Moq;
+using RestauranteAkiWeb.Controllers;
+using RestauranteAkiWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestauranteAkiWeb.Tests.Controllers
+{
+    [TestClass]
+    public class ItemcardapioControllerTests
+    {
+        private static ItemcardapioController controller;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var mockService = new Mock<IItemcardapioService>();
+
+            IMapper mapper = new MapperConfiguration(cfg =>
+                cfg.CreateMap<Itemcardapio, ItemcardapioViewModel>().ReverseMap()).CreateMapper();
+
+            mockService.Setup(service => service.GetAll())
+                .Returns(GetTestItens());
+            mockService.Setup(service => service.Get(1))
+                .Returns(GetTargetItem());
+            mockService.Setup(service => service.Create(It.IsAny<Itemcardapio>()))
+                .Verifiable();
+            mockService.Setup(service => service.Edit(It.IsAny<Itemcardapio>()))
+                .Verifiable();
+            mockService.Setup(service => service.Delete(It.IsAny<int>()))
+                .Verifiable();
+
+            controller = new ItemcardapioController(mapper, mockService.Object);
+        }
+
+        [TestMethod]
+        public void IndexTest_Valido()
+        {
+            var result = controller.Index();
+
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            var viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.Model, typeof(List<ItemcardapioViewModel>));
+
+            var lista = (List<ItemcardapioViewModel>)viewResult.Model;
+            Assert.AreEqual(3, lista.Count);
+        }
+
+        [TestMethod]
+        public void DetailsTest_Valido()
+        {
+            var result = controller.Details(1);
+
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            var viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.Model, typeof(ItemcardapioViewModel));
+
+            var model = (ItemcardapioViewModel)viewResult.Model;
+            Assert.AreEqual("Pizza", model.Nome);
+            Assert.AreEqual(25.0, model.PrecoUnitario);
+        }
+
+        [TestMethod]
+        public void CreateTest_Get_Valido()
+        {
+            var result = controller.Create();
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void CreateTest_Post_Valido()
+        {
+            var result = controller.Create(GetNewItem());
+
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            var redirect = (RedirectToActionResult)result;
+            Assert.AreEqual("Index", redirect.ActionName);
+        }
+
+        [TestMethod]
+        public void CreateTest_Post_Invalido()
+        {
+            controller.ModelState.AddModelError("Nome", "Campo requerido");
+
+            var result = controller.Create(GetNewItem());
+
+            Assert.AreEqual(1, controller.ModelState.ErrorCount);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void EditTest_Get_Valido()
+        {
+            var result = controller.Edit(1);
+
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            var viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.Model, typeof(ItemcardapioViewModel));
+
+            var model = (ItemcardapioViewModel)viewResult.Model;
+            Assert.AreEqual("Pizza", model.Nome);
+        }
+
+        [TestMethod]
+        public void EditTest_Post_Valido()
+        {
+            var model = GetTargetItemViewModel();
+            var result = controller.Edit(model.Id, model);
+
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            var redirect = (RedirectToActionResult)result;
+            Assert.AreEqual("Index", redirect.ActionName);
+        }
+
+        [TestMethod]
+        public void DeleteTest_Get_Valido()
+        {
+            var result = controller.Delete(1);
+
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            var viewResult = (ViewResult)result;
+            var model = (ItemcardapioViewModel)viewResult.Model;
+            Assert.AreEqual("Pizza", model.Nome);
+        }
+
+        [TestMethod]
+        public void DeleteTest_Post_Valido()
+        {
+            var model = GetTargetItemViewModel();
+            var result = controller.Delete(model.Id, model);
+
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            var redirect = (RedirectToActionResult)result;
+            Assert.AreEqual("Index", redirect.ActionName);
+        }
+
+        private ItemcardapioViewModel GetNewItem()
+        {
+            return new ItemcardapioViewModel
+            {
+                Id = 4,
+                Nome = "Hamburguer",
+                PrecoUnitario = 15
+            };
+        }
+
+        private static Itemcardapio GetTargetItem()
+        {
+            return new Itemcardapio
+            {
+                Id = 1,
+                Nome = "Pizza",
+                PrecoUnitario = 25
+            };
+        }
+
+        private ItemcardapioViewModel GetTargetItemViewModel()
+        {
+            return new ItemcardapioViewModel
+            {
+                Id = 1,
+                Nome = "Pizza",
+                PrecoUnitario = 25
+            };
+        }
+
+        private IEnumerable<Itemcardapio> GetTestItens()
+        {
+            return new List<Itemcardapio>
+            {
+                new Itemcardapio { Id = 1, Nome = "Pizza", PrecoUnitario = 25 },
+                new Itemcardapio { Id = 2, Nome = "Coxinha", PrecoUnitario = 5 },
+                new Itemcardapio { Id = 3, Nome = "Refrigerante", PrecoUnitario = 7 }
+            };
+        }
+    }
+}
---
+++ Site/RestauranteAki/ServiceTests/ItemcardapioServiceTests.cs
@@ -0,0 +1,91 @@
+﻿using Core;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Service.Tests
+{
+    [TestClass()]
+    public class ItemcardapioServiceTests
+    {
+        private RestauranteAkiContext context;
+        private IItemcardapioService itemcardapioService;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var builder = new DbContextOptionsBuilder<RestauranteAkiContext>();
+            builder.UseInMemoryDatabase("RestauranteAki");
+            var options = builder.Options;
+
+            context = new RestauranteAkiContext(options);
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            var itens = new List<Itemcardapio>
+            {
+                new() { Id = 1, Nome = "Pizza", PrecoUnitario = (float)35.0m },
+                new() { Id = 2, Nome = "Hamburguer", PrecoUnitario = (float)25.0m },
+                new() { Id = 3, Nome = "Salada", PrecoUnitario = 15 }
+            };
+
+            context.AddRange(itens);
+            context.SaveChanges();
+
+            itemcardapioService = new ItemcardapioService(context);
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            itemcardapioService.Create(new Itemcardapio() { Id = 4, Nome = "Lasanha", PrecoUnitario = (float)40.0M });
+            Assert.AreEqual(4, itemcardapioService.GetAll().Count());
+            var item = itemcardapioService.Get(4);
+            Assert.AreEqual("Lasanha", item.Nome);
+            Assert.AreEqual((float)40.0M, item.PrecoUnitario);
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            itemcardapioService.Delete(2);
+            Assert.AreEqual(2, itemcardapioService.GetAll().Count());
+            var item = itemcardapioService.Get(2);
+            Assert.IsNull(item);
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            var item = itemcardapioService.Get(3);
+            item.Nome = "Salada Caesar";
+            item.PrecoUnitario = 18;
+            itemcardapioService.Edit(item);
+
+            item = itemcardapioService.Get(3);
+            Assert.IsNotNull(item);
+            Assert.AreEqual("Salada Caesar", item.Nome);
+            Assert.AreEqual(18, item.PrecoUnitario);
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            var item = itemcardapioService.Get(1);
+            Assert.IsNotNull(item);
+            Assert.AreEqual("Pizza", item.Nome);
+            Assert.AreEqual(35, item.PrecoUnitario);
+        }
+
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            var listaItens = itemcardapioService.GetAll();
+            Assert.IsInstanceOfType(listaItens, typeof(IEnumerable<Itemcardapio>));
+            Assert.IsNotNull(listaItens);
+            Assert.AreEqual(3, listaItens.Count());
+            Assert.AreEqual(1, listaItens.First().Id);
+            Assert.AreEqual("Pizza", listaItens.First().Nome);
+        }
+    }
+}
