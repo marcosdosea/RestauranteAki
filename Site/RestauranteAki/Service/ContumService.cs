f739d3ab4796586531383a05b191c1b7e0796f01
Pedro Dantas <pedrood1232@gmail.com>
2025-08-29 15:48:05 -0300

    Implementado Service para caso de uso Encerrar Conta. Close #236

--- Site/RestauranteAki/Service/ContumService.cs
+++ Site/RestauranteAki/Service/ContumService.cs
@@ -1,33 +1,75 @@
 ﻿using Core;
 using Core.Service;
+using Microsoft.EntityFrameworkCore;
 
 namespace Service
 {
+    /// <summary>
+    /// Implementa serviços para manter dados da conta
+    /// </summary>
     public class ContumService : IContumService
     {
-        public int Create(Contum contum)
+
+        private readonly RestauranteAkiContext context;
+
+        public ContumService(RestauranteAkiContext context)
         {
-            throw new NotImplementedException();
+            this.context = context;
         }
 
+        /// <summary>
+        /// Criar uma nova conta na base de dados
+        /// </summary>
+        /// <param name="conta">dados da conta</param>
+        /// <returns>id da conta</returns>
+        public int Create(Contum conta)
+        {
+            context.Add(conta);
+            context.SaveChanges();
+            return conta.Id;
+        }
+
+        /// <summary>
+        /// Remover a conta da base de dados
+        /// </summary>
+        /// <param name="id">id da conta</param>
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            var conta = context.Conta.Find(id);
+            if (conta != null)
+            {
+                context.Remove(conta);
+                context.SaveChanges();
+            }
         }
 
-        public void Edit(Contum contum)
+        /// <summary>
+        /// Editar dados da conta na base de dados
+        /// </summary>
+        /// <param name="conta"></param>
+        public void Edit(Contum conta)
         {
-            throw new NotImplementedException();
+            context.Update(conta);
+            context.SaveChanges();
         }
 
+        /// <summary>
+        /// Buscar uma conta na base de dados
+        /// </summary>
+        /// <param name="id">id da conta</param>
+        /// <returns>dados da conta</returns>
         public Contum? Get(int id)
         {
-            throw new NotImplementedException();
+            return context.Conta.Find(id);
         }
 
+        /// <summary>
+        /// Buscar todas as contas presentes na base de dados
+        /// </summary>
+        /// <returns>lista de contas</returns>
         public IEnumerable<Contum> GetAll()
         {
-            throw new NotImplementedException();
+            return context.Conta.AsNoTracking();
         }
     }
 }
