8b8fbc1dd6d17303246f06cfe316305ee8f19b77
Pedro Dantas <pedrood1232@gmail.com>
2025-09-18 02:19:51 -0300

    Correçao de logica do controller/views e css de Pessoa. Close #299

--- Site/RestauranteAki/RestauranteAkiWeb/Models/PessoaViewModel.cs
+++ Site/RestauranteAki/RestauranteAkiWeb/Models/PessoaViewModel.cs
@@ -1,73 +1,80 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using Core;
 
 namespace RestauranteAkiWeb.Models
 {
+    public enum TipoPessoa
+    {
+        Funcionario = 'F',
+        Gestor = 'G'
+    }
+
     public class PessoaViewModel
     {
-        [Display(Name = "Código")]
-        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         public int Id { get; set; }
 
         [Display(Name = "Nome Completo")]
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(50)]
         public string NomeCompleto { get; set; } = string.Empty;
 
         [Display(Name = "CPF")]
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(14)]
         public string Cpf { get; set; } = string.Empty;
 
+        [Display(Name = "Data de Nascimento")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [DataType(DataType.Date)]
+        public DateTime DataNascimento { get; set; }
+
         [Display(Name = "E-mail")]
+        [EmailAddress(ErrorMessage = "O e-mail informado não é válido.")]
+        [StringLength(50)]
         public string? Email { get; set; }
 
         [Display(Name = "Telefone")]
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(50)]
         public string Telefone { get; set; } = string.Empty;
-
-        [Display(Name = "Data de Nascimento")]
-        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
-        public DateTime DataNascimento { get; set; }
 
         [Display(Name = "CEP")]
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(20)]
         public string Cep { get; set; } = string.Empty;
 
         [Display(Name = "Logradouro")]
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(50)]
         public string Logradouro { get; set; } = string.Empty;
 
         [Display(Name = "Bairro")]
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(50)]
         public string Bairro { get; set; } = string.Empty;
 
         [Display(Name = "Cidade")]
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(50)]
         public string Cidade { get; set; } = string.Empty;
 
         [Display(Name = "Estado")]
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(50)]
         public string Estado { get; set; } = string.Empty;
 
         [Display(Name = "Complemento")]
+        [StringLength(50)]
         public string? Complemento { get; set; }
 
         [Display(Name = "Foto")]
-        public byte[]? Foto { get; set; }
+        public IFormFile? Foto { get; set; }
 
-        [Display(Name = "Restaurante")]
+        [Display(Name = "Restaurante associado")]
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         public int IdRestaurante { get; set; }
 
-        /// <summary>
-        /// F - Funcionário
-        /// G - Gestor
-        /// </summary>
         [Display(Name = "Tipo de Pessoa")]
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
-        public string TipoPessoa { get; set; } = string.Empty;
-
-        public Restaurante? IdRestauranteNavigation { get; set; }
-
-        public ICollection<Pedido>? Pedidos { get; set; } 
+        public TipoPessoa TipoPessoa { get; set; }
     }
 }
